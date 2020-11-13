270bb1aa4cd72c64326bab514367ef56129a4d2c
ramimhossain12 <ramimhossain6242@gmail.com>
2020-11-13 19:10:41 +0600

    All Data updated

--- hello.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("connection.php");
-$query = "INSERT INTO STUDENT VALUES('4','Rakib khan','BBA')";
-$data = mysqli_query($conn, $query);
-
-if($data){
-    echo "Data inserted into database";
-}
-?>
